b88bed1c93e9b8b1f53de544ae5108a37da96fd3
RagdollGoDown <zenooby2002@gmail.com>
2025-05-16 22:22:32 +0200

    add: working blip test

--- test_blip.py
+++ test_blip.py
@@ -1,21 +1,31 @@
 # Load model directly
-from transformers import AutoProcessor, AutoModelForImageTextToText
+from PIL import Image
+from transformers import AutoProcessor, BlipForQuestionAnswering
+import os
 
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = AutoModelForImageTextToText.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = AutoProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 
-def query_blip(image_path):
+def query_blip(image, question="How does this dog feel?"):
     # Load and process the image
-    image = processor(images=image_path, return_tensors="pt").pixel_values
+    inputs = processor(images=image,text=question ,return_tensors="pt")
 
     # Generate caption
-    outputs = model.generate(image)
+    outputs = model.generate(**inputs)
     caption = processor.batch_decode(outputs, skip_special_tokens=True)[0]
     
     return caption
 
 def main():
     # Example usage
-    image_path = "assets/images/happy_dog.jpeg"  # Replace with your image path
-    caption = query_blip(image_path)
+    image_path = os.getcwd() + "\\assets\\images\\happy_dog.jpeg"
+    # Load the image
+    image = Image.open(image_path)
+
+    question = "What does it inspire in you?"
+
+    caption = query_blip(image, question)
     print("Generated Caption:", caption)
+
+if __name__ == "__main__":
+    main()
