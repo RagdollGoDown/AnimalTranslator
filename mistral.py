9d7e03b85667144e3311bf17a06671792aeeb3d3
Erik Hübner <erik.hubner@protonmail.ch>
2025-05-17 13:52:03 +0200

    feat: implement new text to text function

--- mistral.py
+++ mistral.py
@@ -145,14 +145,15 @@ model = "mistral-large-latest"
 
 client = Mistral(api_key=api_key)
 
-chat_response = client.chat.complete(
-    model = model,
-    messages = [
-        {
-            "role": "user",
-            "content": "What is the best French cheese?",
-        },
-    ]
-)
 
-print(chat_response.choices[0].message.content)
+def ask_mistral(question):
+    chat_response = client.chat.complete(
+        model = model,
+        messages = [
+            {
+                "role": "user",
+                "content": f"{question}",
+            },
+        ]
+    )
+    return chat_response.choices[0].message.content
