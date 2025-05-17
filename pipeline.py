694cbc81d5e7a1356e9afffb3b88628a5d468469
RagdollGoDown <zenooby2002@gmail.com>
2025-05-17 14:30:48 +0200

    add: different pipelines

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 import librosa
 
-def query_image(image_path, **questions):
+def query_image(image_path, questions):
     # Load and process the image
     answers = image_and_text_to_text(image_path, questions)
     return answers
@@ -17,7 +17,6 @@ def query_image_blip(image, questions):
 
     for question in questions:
         answers.append(query_blip(image, question))
-        print(f"Question: {question}\nAnswer: {answers}\n")
     return answers
 
 
@@ -25,12 +24,10 @@ def speech_recognition(audio):
     result = query_whisper(audio)
     return result
 
-def pipeline(image_path, audio) -> str:
+def pipeline_full(image_path, audio) -> str:
 
     image_questions = [
-        "What is the animal in the image?",
-        "How is the animal feeling?",
-        "What color is this animal?"
+        "Give me a descritpition of the animal in the image, it's emotional state and what it's doing.",
     ]
 
     image = Image.open(image_path)
@@ -58,7 +55,57 @@ def pipeline(image_path, audio) -> str:
     {result}
 
     You are analyzing an animal in an image and a sound. We are going to give information retrieved from both.
+    The informat from the image is a reliable and can appear in your answer, but the information from the sound is not reliable.
+    The speech recognition should only serve as inspiration for your answer and not appear in it.
+    Give a plausible answer with confidence.
+    Keep it short and simple.
+    \n
+    """
+    
+    final_text = ask_mistral(final_text_question)
+
+    return final_text
+
+def pipeline_audio(audio) -> str:
+    # audio -> transcribe
+    result = speech_recognition(audio)
+    print("Animal speech detected:", result)
+
+    # information aggregation
+    final_text_question = f"""
+    The information retrieved from the sound is:\n
+    {result}
+
+    You are analyzing an animal in a sound. 
     The information retrieved from the sound is a speech recognition result. 
+    Do not mention it and only use it as inspiration for your answer.
+    You do not have to actually tell the truth about the animal, just give a plausible answer with confidence.
+    \n
+    """
+    
+    final_text = ask_mistral(final_text_question)
+
+    return final_text
+
+def pipeline_image(image_path) -> str:
+    image_questions = [
+        "Give me a descritpition of the animal in the image, it's emotional state and what it's doing.",
+    ]
+
+    # Load and process the image
+    answers_image = query_image(image_path, questions=image_questions)
+    
+    image_info_block = ""
+    for question, answer in zip(image_questions, answers_image):
+        image_info_block += f"Question: {question}\nAnswer: {answer}\n\n"
+    print("Image information block:", image_info_block)
+
+    # information aggregation
+    final_text_question = f"""
+    The information retrieved from the image is:\n
+    {image_info_block}
+    
+    You are analyzing an animal in an image. 
     Do not mention it and only use it as inspiration for your answer.
     You do not have to actually tell the truth about the animal, just give a plausible answer with confidence.
     \n
@@ -80,7 +127,7 @@ def main():
     # Load the audio
     audio, sr = librosa.load(audio_path, sr=16000)
 
-    final_text = pipeline(image_path, audio)
+    final_text = pipeline_full(image_path, audio)
     print("Final text:", final_text)
 
 if __name__ == "__main__":
